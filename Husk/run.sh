862f7894662e24bf5fe55511d95905d1b7fca727
Hydrazer <explodingaerosal@gmail.com>
2021-12-02 05:12:42 -0700

    added real husk

--- Husk/run.sh
+++ Husk/run.sh
@@ -3,6 +3,6 @@ args="$2"
 input=`cat 'input.txt'`
 code_path="$PWD"/code.husk
 
-cd husk
+cd src
 
 ./Husk -uf "$code_path" "$args"
--- replit.nix
+++ replit.nix
@@ -1,5 +1,17 @@
 { pkgs }: {
   deps = [
+    pkgs.libffi-dev
+    pkgs.libffi 
     pkgs.php74
+    haskellPackages.libffi
+    haskellPackages.libffi-dev
+    nixos.haskellPackages.libffi
+    nixos.haskellPackages.libffi-dev
+    nixos.libffi
+    nixos.libffi-dev
+    pkgs.nixos.libffi
+    pkgs.nixos.libffi-dev
+    pkgs.nixos.haskellPackages.libffi
+    pkgs.nixos.haskellPackages.libffi-dev
   ];
 }
